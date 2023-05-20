e1160935acd4e782ca9ec3ed280f22cd4008f59f
Pedro Duarte <pcduarte@edu.unifor.br>
2023-05-19 22:35:29 -0300

    teste2

--- src/WebScrapping/Main.php
+++ src/WebScrapping/Main.php
@@ -9,12 +9,9 @@ use DOMDocument;
  */
 class Main {
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
   public static function run(): void {
     $dom = new DOMDocument('1.0', 'utf-8');
-    @$dom->loadHTMLFile(__DIR__ . '/../../webscrapping/origin.html'); // coloquei o @ temporariamente para poder trabalhar melhor com o documento sem warnings
+    @$dom->loadHTMLFile(__DIR__ . '/../../webscrapping/origin.html'); // coloqemporariamente para poder trabalhar melhor com o documento sem warnings
     (new Scrapper())->scrap($dom);
   }
 
